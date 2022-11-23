72ad6498673af401d1404679a2a793e041768e9c
user <ujjalroy7862@gmail.com>
2022-11-24 00:57:21 +0600

    try to solve but its little hard for me

--- LeetCodeProblem/48Problem.js
+++ LeetCodeProblem/48Problem.js
@@ -10,15 +10,13 @@ for (let i = 0; i < rows; i++) {
 
 for (let i = 0; i < rows; i++) {
   for (let j = 0; j < cols; j++) {
-    Array2d[i][j] = Number(
-      prompt(`Enther the value i postition ${i} and j: ${j}`)
-    );
+    Array2d[i][j] = Number(prompt(`${i} , ${j} = `));
   }
 }
+console.log("\n");
 
 for (let i = 0; i < rows; i++) {
   for (let j = 0; j < cols; j++) {
-    console.log(`[${Array2d[i][j]}]`);
+    console.log(Array2d[i] + "+" + Array2d[j]);
   }
-  console.log("\n");
 }
