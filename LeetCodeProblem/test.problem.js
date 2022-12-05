c76c03e3b6473082085565a76487b150e48be445
user <ujjalroy7862@gmail.com>
2022-12-05 22:29:11 +0600

    now my solution is very efficient

--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,14 +1,14 @@
 var singleNumber = function (nums) {
   const ht = {};
-
   for (let num of nums) {
-    console.log(ht[num] = ht[num] + 1 || 1);
+    console.log(num);
+    console.log((ht[num] = ht[num] + 1 || 1));
   }
-
+  console.log(ht);
   for (let key in ht) {
     if (ht[key] === 1) {
       console.log(key);
     }
   }
 };
-singleNumber([2,2,1,1])
+singleNumber([4, 2, 2, 1, 1, 1]);
