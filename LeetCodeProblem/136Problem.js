a5c36cbff1787bf41aacf0e0ab4792b87270fab9
user <ujjalroy7862@gmail.com>
2022-12-05 22:30:07 +0600

    now my solution is very efficient

--- LeetCodeProblem/136Problem.js
+++ LeetCodeProblem/136Problem.js
@@ -1,13 +1,15 @@
 var singleNumber = function (nums) {
-
-  for (let i = 0; i < nums.length; i++) {
-    if (i === nums.lastIndexOf(nums[i])) {
-      return nums[i];
-    } else {
-      nums.splice(nums.lastIndexOf(nums[i]), 1);
+  const ht = {};
+  for (let num of nums) {
+    console.log(num);
+    console.log((ht[num] = ht[num] + 1 || 1));
+  }
+  console.log(ht);
+  for (let key in ht) {
+    if (ht[key] === 1) {
+      console.log(key);
     }
   }
-
 };
 
 console.log(singleNumber([4, 1, 2, 1, 2]));
