d7cd61e15a40f20ca0d82354080d8ef9c6fcba58
user <ujjalroy7862@gmail.com>
2022-12-08 21:53:13 +0600

    very small problem

---
+++ LeetCodeProblem/70Problem.js
@@ -0,0 +1,13 @@
+var climbStairs = function (n) {
+  if (n === 2) {
+    return n;
+  } else if (1 + 1 + 1 === n) {
+    return n;
+  } else if (1 + 2 === n) {
+    return n;
+  } else if (2 + 1 === n) {
+    return n;
+  } else {
+    return 1;
+  }
+};
--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,13 +1,5 @@
-var rotate = function(matrix) {
-  matrix = matrix.reverse();
-
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < i; j++) {
-      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
-    }
-  }
-
-  console.log(matrix)
+var isPerfectSquare = function (num) {
+  return Number.isInteger(Math.sqrt(num));
 };
 
-rotate([[1,2,3],[4,5,6],[7,8,9]])
+isPerfectSquare(14);
