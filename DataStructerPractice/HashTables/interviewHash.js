2ff9d0365551b4c05f1ba9d6610891c9f7fa140d
user <ujjalroy7862@gmail.com>
2022-12-14 18:22:08 +0600

    this is very time consuiming solution and time complexity is n^2

--- DataStructerPractice/HashTables/interviewHash.js
+++ DataStructerPractice/HashTables/interviewHash.js
@@ -1,12 +1,14 @@
 const firstRecurr = (arrays) => {
-  for (let i = 0; i < arrays.length; i++) {
-    for (let j = i + 1; j < arrays.length; j++) {
-      if (arrays[i] === arrays[j]) {
-        return arrays[i];
-      }
-    }
-  }
-  return undefined;
+
+  //This is very unefficent solution because of time complexity is N^2
+  // for (let i = 0; i < arrays.length; i++) {
+  //   for (let j = i + 1; j < arrays.length; j++) {
+  //     if (arrays[i] === arrays[j]) {
+  //       return arrays[i];
+  //     }
+  //   }
+  // }
+  // return undefined;
 };
 
 console.log(firstRecurr([2, 5, 1, 2, 3, 5, 1, 2, 4]));
