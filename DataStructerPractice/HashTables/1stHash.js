0d647ed891e6859a93d4336062a0beea5be6c38b
user <ujjalroy7862@gmail.com>
2022-12-14 17:51:10 +0600

    build my first hash table

--- DataStructerPractice/HashTables/1stHash.js
+++ DataStructerPractice/HashTables/1stHash.js
@@ -3,15 +3,40 @@ class HashTable {
     this.data = new Array(size);
   }
 
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
   set(key, value) {
-    this.data = key
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+      this.data[address].push([key, value]);
+    } else {
+      this.data[address].push([key, value]);
+      return this.data;
+    }
   }
 
   get(key) {
-
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
   }
 }
 
 const myHashTable = new HashTable(50);
 myHashTable.set("grapes", 10000);
-myHashTable.get("grapes");
+myHashTable.set("mango", 54);
+console.log(myHashTable.get("mango"));
