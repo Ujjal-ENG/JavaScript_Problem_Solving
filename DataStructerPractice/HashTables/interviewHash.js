1bee5766d6fdc5c847a42bea59dcbb6d58752183
user <ujjalroy7862@gmail.com>
2022-12-14 20:29:19 +0600

    now this is veey first problem

--- DataStructerPractice/HashTables/interviewHash.js
+++ DataStructerPractice/HashTables/interviewHash.js
@@ -1,5 +1,4 @@
 const firstRecurr = (arrays) => {
-
   //This is very unefficent solution because of time complexity is N^2
   // for (let i = 0; i < arrays.length; i++) {
   //   for (let j = i + 1; j < arrays.length; j++) {
@@ -9,6 +8,19 @@ const firstRecurr = (arrays) => {
   //   }
   // }
   // return undefined;
+
+  let ht = {};
+
+  for (let i in arrays) {
+    if (ht[arrays[i]] !== undefined) {
+      return arrays[i];
+    } else {
+      ht[arrays[i]] = i;
+    }
+    console.log(ht)
+  }
+
+  return undefined;
 };
 
-console.log(firstRecurr([2, 5, 1, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurr([2, 5, 7, 1, 3, 5, 1, 2, 4]));
