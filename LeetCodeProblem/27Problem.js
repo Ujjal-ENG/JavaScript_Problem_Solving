35ecc5b5177610a66bc6809a06c582c66b10831e
user <ujjalroy7862@gmail.com>
2022-12-21 12:53:51 +0600

    finally this solution leetcode taken

--- LeetCodeProblem/27Problem.js
+++ LeetCodeProblem/27Problem.js
@@ -13,14 +13,19 @@
 //   return nums;
 // };
 
-var removeElement = function (nums, val) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === val) {
-      nums.splice(i, 1)
-      i--
-    }
+// var removeElement = function (nums, val) {
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === val) {
+//       nums.splice(i, 1)
+//       i--
+//     }
+//   }
+//   return nums
+// }
+var removeElement = function(nums, val) {
+  for (let i = nums.length - 1; i > -1; i--) {
+    if (nums[i] === val) nums.splice(i, 1);
   }
-  return nums
-}
+};
 
 console.log(removeElement([3, 2, 2, 3], 3));
