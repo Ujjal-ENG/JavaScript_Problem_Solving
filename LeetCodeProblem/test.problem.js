69d4ee350ab79f527b0dc048f035b9222358b646
user <ujjalroy7862@gmail.com>
2022-12-22 03:05:57 +0600

    try to solve another problem but i failed

--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,14 +1,13 @@
-var sumOfUnique = function (nums) {
+var digitCount = function (nums) {
   const ht = {};
   for (let i of nums) {
     ht[i] = ht[i] + 1 || 1;
   }
-  let sum = 0;
   for (i in ht) {
-    if (ht[i] === 1) {
-      sum += Number(i);
+    if (ht[0] >=1) {
+      return false;
     }
   }
-  return sum;
+  return true;
 };
-console.log(sumOfUnique([1, 2, 3, 2]));
+console.log(digitCount("0"));
