f0ac44ceb1bb35cd76297f565a88616b9ea72d9c
user <ujjalroy7862@gmail.com>
2022-12-21 12:50:23 +0600

    this is the another solution this problem

--- LeetCodeProblem/27Problem.js
+++ LeetCodeProblem/27Problem.js
@@ -1,16 +1,26 @@
+// var removeElement = function (nums, val) {
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === val) {
+//       nums[i] = "_";
+//     }
+//   }
+//   nums.sort();
+
+//   for (let i = 0; i < nums.length; i++) {
+//     nums.pop("_")
+//   }
+
+//   return nums;
+// };
+
 var removeElement = function (nums, val) {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === val) {
-      nums[i] = "_";
+      nums.splice(i, 1)
+      i--
     }
   }
-  nums.sort();
-
-  for (let i = 0; i < nums.length; i++) {
-    nums.pop("_")
-  }
-
-  return nums;
-};
+  return nums
+}
 
 console.log(removeElement([3, 2, 2, 3], 3));
