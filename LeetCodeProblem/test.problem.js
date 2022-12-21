888c05f82eed968d2890f99b9322c6d2f8e3ff99
user <ujjalroy7862@gmail.com>
2022-12-22 02:53:03 +0600

    solve this problem

--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,11 +1,14 @@
-var maximumWealth = function (accounts) {
-  return Math.max(
-    ...accounts.map((account) => account.reduce((prev, curr) => prev + curr), 0)
-  );
+var sumOfUnique = function (nums) {
+  const ht = {};
+  for (let i of nums) {
+    ht[i] = ht[i] + 1 || 1;
+  }
+  let sum = 0;
+  for (i in ht) {
+    if (ht[i] === 1) {
+      sum += Number(i);
+    }
+  }
+  return sum;
 };
-console.log(
-  maximumWealth([
-    [1, 2, 3],
-    [3, 2, 1],
-  ])
-);
+console.log(sumOfUnique([1, 2, 3, 2]));
