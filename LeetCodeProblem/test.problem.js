bea4264bf4626336b88913b733f51359ff6232c6
user <ujjalroy7862@gmail.com>
2022-12-23 15:05:02 +0600

    in my compiler its solved but why it can take leetcode i don't know

--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,8 +1,13 @@
 var search = function (nums, target) {
+  let findIdx = [];
   if (nums.includes(target)) {
-    let index = nums.indexOf(target);
-    return index;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === target) {
+        findIdx.push(i);
+      }
+    }
+    return findIdx;
   }
-  return -1;
+  return [-1, -1];
 };
-console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
+console.log(search([5, 7, 7, 8, 8, 10], 6));
