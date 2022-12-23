e34d3d7e2f3b02e46217bb4606ada35c2a35f3e0
user <ujjalroy7862@gmail.com>
2022-12-24 01:47:52 +0600

    Try to solve this problem but i failed

--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,6 +1,24 @@
-var lengthOfLastWord = function (nums) {
-  let take = nums.trim();
-  let newTake = take.split(" ");
-  return newTake[newTake.length - 1].length;
+var isIsomorphic = function (s, t) {
+  const ht = {};
+
+  if (s === t) {
+    return true
+  }
+  for (let i of s) {
+    ht[i] = ht[i] + 1 || 1;
+  }
+  for (let j of t) {
+    ht[j] = ht[j] + 1 || 1;
+  }
+  let count = 0;
+  for (let i in ht) {
+    if (ht[i] > 1) {
+      count++;
+    }
+  }
+  if (count === 1) {
+    return false;
+  }
+  return true;
 };
-console.log(lengthOfLastWord("   fly me   to   the moon  "));
+console.log(isIsomorphic("a", "a"));
