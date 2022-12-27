a8acccc3eedc1b820691424a713a6adbe74cfa6a
user <ujjalroy7862@gmail.com>
2022-12-28 00:10:53 +0600

    this is a very trick and hard problem

--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,10 +1,23 @@
-var combinationSum = function (candidates, target) {
-  let sum = candidates.reduce((prev, curr) => {
-    if ((prev + curr) === target) {
-      return prev + curr;
-    }
-  }, 0);
+var firstMissingPositive = function (nums) {
+  let newArray = nums.sort((a, b) => b - a);
+  let maxNum = nums[0];
+  const ht = {};
+  let newNum = 1;
+  for (let i = 0; i <= maxNum; i++) {
+    ht[i] = ht[i];
+  }
+  for (let i of nums) {
+    ht[i] = ht[i] || 1;
+  }
 
-  return sum;
+  for (let i in ht) {
+    if (ht[i] === undefined) {
+      if (Number(i) === 1) {
+        return 1;
+      }
+    } else {
+      return maxNum + 1;
+    }
+  }
 };
-console.log(combinationSum([2, 3, 6, 7], 7));
+console.log(firstMissingPositive([3,4,-1,1]));
