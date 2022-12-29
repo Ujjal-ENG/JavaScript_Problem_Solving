248824f0d2897412cdbcf6d95efe7d2ae6fe63c7
user <ujjalroy7862@gmail.com>
2022-12-30 03:48:25 +0600

    good one

--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,21 +1,40 @@
-var containsDuplicate = function (nums) {
-  const ht = {};
-  for (let i of nums) {
-    ht[i] = ht[i] + 1 || 1;
-  }
-  for (let i in ht) {
-    if (ht[i] > 1) {
-      return true;
+var romanToInt = function (s) {
+  let sum = 0;
+  let obj = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  // while (s) {
+  //   for (let key in obj) {
+  //     if (s.startsWith(key)) {
+  //       sum += obj[key];
+  //       s = s.slice(key.length);
+  //       console.log("Changes of the S value: ", s,key);
+  //     }
+  //   }
+  // }
+
+  while (s) {
+    for (let key in obj) {
+      if (s.startsWith(key)) {
+        sum += obj[key];
+        s = s.slice(key.length);
+      }
     }
   }
-  return false;
+  return sum;
 };
-console.log(
-  containsDuplicate([
-    7, 10, 5, 5, 6, 6, 4, 10, 5, 4, 9, 4, 9, 6, 5, 9, 6, 3, 6, 5, 6, 7, 7, 4, 9,
-    9, 10, 5, 8, 1, 8, 3, 2, 7, 5, 10, 1, 8, 5, 8, 4, 3, 6, 4, 9, 4, 2, 8, 3, 2,
-    2, 1, 5, 6, 3, 2, 6, 1, 8, 6, 2, 9, 1, 4, 5, 10, 8, 5, 10, 5, 10, 1, 4, 8,
-    3, 6, 4, 10, 9, 1, 1, 1, 2, 2, 9, 6, 6, 8, 1, 9, 2, 5, 5, 2, 1, 8, 5, 2, 3,
-    10,
-  ])
-);
+
+console.log(romanToInt("LVIII"));
