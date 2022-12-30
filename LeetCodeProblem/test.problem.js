fc00990ebe35105bc7a95b2f36c614dd03495a3a
user <ujjalroy7862@gmail.com>
2022-12-30 20:12:58 +0600

    easy problem

--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,32 +1,10 @@
-var intToRoman = function (num) {
-  let res = "";
-  if (num < 1 || num > 3999) return res;
-  let weight = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  let token = [
-    "M",
-    "CM",
-    "D",
-    "CD",
-    "C",
-    "XC",
-    "L",
-    "XL",
-    "X",
-    "IX",
-    "V",
-    "IV",
-    "I",
-  ];
-  let i = 0;
-  while (num > 0) {
-    if (num - weight[i] >= 0) {
-      res += token[i];
-      num -= weight[i];
-    } else {
-      i++;
-    }
+var shuffle = function (nums, n) {
+  const res = [];
+  for (let i = 0; i < n; i++) {
+    res.push(nums[i]);
+    res.push(nums[i + n]);
   }
   return res;
 };
 
-console.log(intToRoman(58));
+console.log(shuffle([2,5,1,3,4,7], 3));
