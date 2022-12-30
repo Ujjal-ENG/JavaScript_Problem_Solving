8b8c3a02d4ca6ab61f0c20ad8ad37aa886bdbee4
user <ujjalroy7862@gmail.com>
2022-12-30 17:42:03 +0600

    no chnage yet

--- LeetCodeProblem/tempCodeRunnerFile.js
+++ LeetCodeProblem/tempCodeRunnerFile.js
@@ -1,1 +1,9 @@
-I
+  // let i = 0;
+  // while (num > 0) {
+  //   if (num - weight[i] >= 0) {
+  //     res += token[i];
+  //     num -= weight[i];
+  //   } else {
+  //     i++;
+  //   }
+  // }
