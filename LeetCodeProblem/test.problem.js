58f2e67e9f1bc197d9002c398b6100990f42014a
user <ujjalroy7862@gmail.com>
2023-01-01 03:08:43 +0600

    solve this problem

--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,17 +1,13 @@
-var tribonacci = function (n) {
-  if (n === 0) return 0;
-  if (n < 3) return 1;
-  // let t0 = 0;
-  // let t1 = 1;
-  // let t2 = 1;
-  // for (let t = 3; t <= n; t++) {
-  //   const tn = t0 + t1 + t2;
-  //   t0 = t1;
-  //   t1 = t2;
-  //   t2 = tn;
-  // }
-  // return t2;
-  return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+var simplifyPath = function (path) {
+  path = path.split("/");
+  let newArray = [];
+  for (let i of path) {
+    if (i == "." || i == "") continue;
+    else if (i == "..") newArray.pop();
+    else newArray.push(i);
+  }
+
+  return "/" + newArray.join("/");
 };
 
-console.log(tribonacci(25));
+console.log(simplifyPath("/home//foo/"));
