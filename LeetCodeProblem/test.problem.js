09a3f15fcf2dc513f70b9532677df5133751e325
user <ujjalroy7862@gmail.com>
2023-01-25 02:34:59 +0600

    try to solve this problem but not yet

--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,4 +1,21 @@
-var findMin = function (nums) {
-  return nums.sort().slice(0, 1).join("");
+var longestConsecutive = function (nums) {
+  let newNum = nums.sort((a, b) => a - b);
+
+  newNum = newNum.filter((x, i) => newNum.indexOf(x) === i);
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (newNum[0] === 0) {
+      if (i === newNum[i]) {
+        count++;
+      }
+    } else {
+      if (i + 1 === newNum[i]) {
+        count++;
+      }
+    }
+  }
+
+  return count;
 };
-console.log(findMin([3, 4, 5, 1, 2]));
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]));
