9d62a5a8c6000180a1d7594ea5964b4c26a3d9a9
user <ujjalroy7862@gmail.com>
2023-02-13 00:44:20 +0600

    now it solved very efficient way

--- DataStructerPractice/samePower.js
+++ DataStructerPractice/samePower.js
@@ -15,19 +15,35 @@
 //   return false;
 // }
 
+// function same(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     let correntIndex = arr2.indexOf(arr1[i] ** 2);
+//     if (correntIndex === -1) {
+//       return false;
+//     }
+//     console.log(arr2);
+//     arr2.splice(correntIndex, 1);
+//   }
+//   return true;
+// }
+
 function same(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    let correntIndex = arr2.indexOf(arr1[i] ** 2);
-    if (correntIndex === -1) {
-      return false;
+  if (arr1.length === arr2.length) {
+    for (let i of arr1) {
+      let currentIndex = arr2.indexOf(i ** 2);
+      if (currentIndex === -1) {
+        return false;
+      }
+      console.log(arr2);
+      arr2.splice(currentIndex, 1);
     }
-    console.log(arr2);
-    arr2.splice(correntIndex, 1);
+    return true;
   }
-  return true;
+
+  return false;
 }
 
-console.log(same([2, 1, 2, 3], [9, 1, 4, 6]));
+console.log(same([2, 1, 3], [9, 1, 4]));
