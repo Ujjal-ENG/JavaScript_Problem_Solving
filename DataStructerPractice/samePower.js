88f0266912e9784bd5ca047c3d3a0ad2d7a6bef8
user <ujjalroy7862@gmail.com>
2023-02-13 00:40:29 +0600

    known something very special

--- DataStructerPractice/samePower.js
+++ DataStructerPractice/samePower.js
@@ -1,14 +1,33 @@
-function same(arr1, arr2) {
-  arr1.sort((a, b) => a - b);
-  arr2.sort((a, b) => a - b);
+// function same(arr1, arr2) {
+//   arr1.sort((a, b) => a - b);
+//   arr2.sort((a, b) => a - b);
 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] ** 2 === arr2[i]) {
-      return true;
-    }
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1.length === arr2.length) {
+//       if (arr1[i] ** 2 === arr2[i]) {
+//         return true;
+//       }
+//       return false;
+//     } else {
+//       return false;
+//     }
+//   }
+//   return false;
+// }
+
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
-  return false;
+  for (let i = 0; i < arr1.length; i++) {
+    let correntIndex = arr2.indexOf(arr1[i] ** 2);
+    if (correntIndex === -1) {
+      return false;
+    }
+    console.log(arr2);
+    arr2.splice(correntIndex, 1);
+  }
+  return true;
 }
 
-console.log(same([4, 2, 3], [9, 1, 4]));
+console.log(same([2, 1, 2, 3], [9, 1, 4, 6]));
