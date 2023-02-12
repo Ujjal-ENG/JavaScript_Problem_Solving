4e2afaf68db78204e6c46980d135ea5abe7344aa
user <ujjalroy7862@gmail.com>
2023-02-13 01:56:05 +0600

    this the the more efficient way to solve this problem

--- DataStructerPractice/sumZero.js
+++ DataStructerPractice/sumZero.js
@@ -1,9 +1,24 @@
 function sumZero(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === 0) {
-        return [nums[i], nums[j]];
-      }
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] === 0) {
+  //       return [nums[i], nums[j]];
+  //     }
+  //   }
+  // }
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let sum = nums[left] + nums[right];
+
+    if (sum === 0) {
+      return [nums[left], nums[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left--;
     }
   }
 }
