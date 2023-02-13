34abb9f13c3569578f3119357999f50235a639e4
user <ujjalroy7862@gmail.com>
2023-02-13 11:54:26 +0600

    now every thing is clear in binary search

--- DataStructerPractice/binarySearch.js
+++ DataStructerPractice/binarySearch.js
@@ -1,16 +1,34 @@
 function binarySearch(arrs, value) {
-  let min = 0;
-  let max = arrs.length - 1;
+  // let min = 0;
+  // let max = arrs.length - 1;
 
-  while (min <= max) {
-    let middle = Math.floor((min + max) / 2);
+  // while (min <= max) {
+  //   let middle = Math.floor((min + max) / 2);
+
+  //   let currentEl = arrs[middle];
+
+  //   if (currentEl < value) {
+  //     min = middle + 1;
+  //   } else if (currentEl > value) {
+  //     max = middle - 1;
+  //   } else {
+  //     return middle;
+  //   }
+  // }
+  // return -1;
+
+  let left = 0;
+  let right = arrs.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
 
     let currentEl = arrs[middle];
 
-    if (currentEl < value) {
-      min = middle + 1;
-    } else if (currentEl > value) {
-      max = middle - 1;
+    if (currentEl > value) {
+      right = middle - 1;
+    } else if (currentEl < value) {
+      left = middle + 1;
     } else {
       return middle;
     }
@@ -18,4 +36,4 @@ function binarySearch(arrs, value) {
   return -1;
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 8], 4));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 8], 9));
