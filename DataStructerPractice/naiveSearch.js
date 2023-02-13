c8b941f4df083cb3bb2c6be294e5c30d56679270
user <ujjalroy7862@gmail.com>
2023-02-13 13:38:42 +0600

    done the naiveSearch

---
+++ DataStructerPractice/naiveSearch.js
@@ -0,0 +1,33 @@
+function naiveSearch(strings, tarStr) {
+  // let count = 0;
+  // for (let i = 0; i < strings.length; i++) {
+  //   for (let j = 0; j < tarStr.length; j++) {
+  //     console.log(tarStr[j], strings[i + j]);
+  //     if (tarStr[j] !== strings[i + j]) {
+  //       console.log("break");
+  //       break;
+  //     }
+  //     if (j === tarStr.length - 1) {
+  //       count++;
+  //     }
+  //   }
+  // }
+  // return count;
+
+  let count = 0;
+  for (let i = 0; i < strings.length; i++) {
+    for (let j = 0; j < tarStr.length; j++) {
+      console.log(tarStr[j], strings[i + j]);
+      if (tarStr[j] !== strings[i + j]) {
+        console.log("break");
+        break;
+      }
+      if (j === tarStr.length - 1) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+console.log(naiveSearch("lorlolie loled", "lol"));
