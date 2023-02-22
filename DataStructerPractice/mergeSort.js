987390e702d6a0dfa0b92b65c7000e6f7b7a8d18
user <ujjalroy7862@gmail.com>
2023-02-22 22:54:33 +0600

    not fully solved yet

--- DataStructerPractice/mergeSort.js
+++ DataStructerPractice/mergeSort.js
@@ -24,13 +24,13 @@
 //   return results;
 // }
 
-function mergeSort(arrs) {
+function mergeSort(left, right) {
   let i = 0;
   let j = 0;
   let results = [];
-  let mid = Math.trunc(arrs.length / 2);
-  let left = arrs.slice(0, mid);
-  let right = arrs.slice(mid, arrs.length);
+  // let mid = Math.trunc(arrs.length / 2);
+  // let left = arrs.slice(0, mid);
+  // let right = arrs.slice(mid, arrs.length);
 
   while (i < left.length && j < right.length) {
     if (left[i] < right[j]) {
@@ -43,14 +43,24 @@ function mergeSort(arrs) {
   }
 
   while (i < left.length) {
-    results.push(left[i])
-    i++
+    results.push(left[i]);
+    i++;
   }
   while (j < right.length) {
-    results.push(right[j])
-    j++
+    results.push(right[j]);
+    j++;
   }
   return results;
 }
 
-console.log(mergeSort([1, 10, 50, 2, 14, 99, 100]));
+function mergeSortRecuring(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+
+  let left = mergeSortRecuring(arr.slice(0, mid));
+  let right = mergeSortRecuring(arr.slice(mid, arr.length));
+
+  return mergeSort(left, right);
+}
+
+console.log(mergeSort([8, 10, 50, 2, 14, 99, 100]));
