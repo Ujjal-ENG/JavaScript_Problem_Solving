bafb76741efd8d9fec364fd00d73097d909889da
user <ujjalroy7862@gmail.com>
2023-02-22 22:42:47 +0600

    done the merge sort using my way

--- DataStructerPractice/mergeSort.js
+++ DataStructerPractice/mergeSort.js
@@ -1,27 +1,56 @@
-function mergeSort(arr1, arr2) {
-  let results = [];
+// function mergeSort(arr1, arr2) {
+//   let results = [];
 
+//   let i = 0;
+//   let j = 0;
+
+//   while (i < arr1.length && j < arr2.length) {
+//     if (arr2[j] > arr1[i]) {
+//       results.push(arr1[i]);
+//       i++;
+//     } else {
+//       results.push(arr2[j]);
+//       j++;
+//     }
+//   }
+//   while (i < arr1.length) {
+//     results.push(arr1[i]);
+//     i++;
+//   }
+//   while (j < arr2.length) {
+//     results.push(arr2[j]);
+//     j++;
+//   }
+//   return results;
+// }
+
+function mergeSort(arrs) {
   let i = 0;
   let j = 0;
+  let results = [];
+  let mid = Math.trunc(arrs.length / 2);
+  let left = arrs.slice(0, mid);
+  let right = arrs.slice(mid, arrs.length);
 
-  while (i < arr1.length && j < arr2.length) {
-    if (arr2[j] > arr1[i]) {
-      results.push(arr1[i]);
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      results.push(left[i]);
       i++;
     } else {
-      results.push(arr2[j]);
+      results.push(right[j]);
       j++;
     }
   }
-  while (i < arr1.length) {
-    results.push(arr1[i]);
-    i++;
+
+  while (i < left.length) {
+    results.push(left[i])
+    i++
   }
-  while (j < arr2.length) {
-    results.push(arr2[j]);
-    j++;
+  while (j < right.length) {
+    results.push(right[j])
+    j++
   }
   return results;
 }
 
-console.log(mergeSort([1, 10, 50], [2, 14, 99, 100]));
+console.log(mergeSort([1, 10, 50, 2, 14, 99, 100]));
