8fb51abb7f9965053824401fc64e526c459830cb
user <ujjalroy7862@gmail.com>
2023-02-23 16:08:12 +0600

    try to solve this problem but failed

--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,10 +1,7 @@
-var mostFrequent = function (nums, key) {
-  for (let i = 0; i < nums.length; i++) {
-    if (i === key) {
-      return nums[i];
-    }
-  }
-  return nums[nums.length - 1];
+var addBinary = function (a, b) {
+  const num1 = Number(parseInt(a, 2).toString(10));
+  const num2 = Number(parseInt(b, 2).toString(10));
+  const res = num1 + num2;
+  return parseInt(res, 10).toString(2);
 };
-
-console.log(mostFrequent([1, 1000, 2], 1000));
+console.log(addBinary("11", "1"));
