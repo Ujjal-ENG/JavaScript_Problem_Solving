d4471c69d1fb4624fcf5a6284e3cded0f2486d5f
user <ujjalroy7862@gmail.com>
2023-02-26 17:43:16 +0600

    done the first part of quick sort

---
+++ DataStructerPractice/quickSort.js
@@ -0,0 +1,19 @@
+function pivot(arr, start = 0, end = arr.length + 1) {
+  const swap = (arr, i, j) => {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  };
+  
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]));
