27dd26d17fce3a4c68303fbcb6f6ac4e8eb1d099
user <ujjalroy7862@gmail.com>
2023-02-26 21:48:18 +0600

    done my version in linked list pop method

--- DataStructerPractice/SingleLinkedList.js
+++ DataStructerPractice/SingleLinkedList.js
@@ -25,18 +25,50 @@ class SinglyLinkedList {
     return this;
   }
 
+  // pop() {
+  //   if (!this.head) return undefined;
+  //   let current = this.head;
+  //   let newTail = current;
+  //   while (current.next) {
+  //     newTail = current;
+  //     current = current.next;
+  //   }
+  //   this.tail = newTail;
+  //   this.tail.next = null;
+  //   this.length--;
+  //   if (this.length === 0) {
+  //     this.head = null;
+  //     this.tail = null;
+  //   }
+  //   return this;
+  // }
+
   pop() {
     if (!this.head) return undefined;
-    
+
+    let currentNode = this.head;
+    let newTail = currentNode;
+    while (currentNode.next) {
+      newTail = currentNode;
+      currentNode = currentNode.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return currentNode;
   }
 
-  traverse() {
-    let current = this.head;
-    while (current) {
-      console.log(current.val);
-      current = current.next;
-    }
-  }
+  // traverse() {
+  //   let current = this.head;
+  //   while (current) {
+  //     console.log(current.val);
+  //     current = current.next;
+  //   }
+  // }
 }
 
 let list = new SinglyLinkedList();
@@ -44,5 +76,6 @@ list.push("Hello");
 list.push("Hell");
 list.push("Hl");
 list.pop();
-list.traverse();
+// list.pop();
+// list.traverse();
 console.log(list);
--- DataStructerPractice/tempCodeRunnerFile.js
+++ DataStructerPractice/tempCodeRunnerFile.js
@@ -1,2 +1,2 @@
-
-  }
+list.pop();
+// list.pop();
