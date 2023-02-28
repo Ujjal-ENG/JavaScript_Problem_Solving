d2983404e3027b9619e33067db3006c77f0d7c66
user <ujjalroy7862@gmail.com>
2023-02-28 15:42:10 +0600

    start to learn the stack using linked list

---
+++ DataStructerPractice/stack.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+}
+
+const newStack = new Stack();
+
+newStack.push("Ujjal");
+newStack.push(23);
+newStack.push("Engineer");
+console.log(newStack);
