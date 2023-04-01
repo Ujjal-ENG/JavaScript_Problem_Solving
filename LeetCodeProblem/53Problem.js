2f77619b24a14f390f523eca7db5a5c29e2c9ca8
Ujjal Kumar Roy <ujjalroy7862@gmail.comy>
2023-04-02 02:39:09 +0600

    but this solution solved all the test cases

--- LeetCodeProblem/53Problem.js
+++ LeetCodeProblem/53Problem.js
@@ -1,27 +1,16 @@
 var maxSubArray = function (nums) {
-  const ht = {};
-  if (nums.length === 1) {
-    return nums[0];
-  }
-  for (let i of nums) {
-    ht[i] = ht[i] + 1 || 1;
-  }
-  let newArray = [];
-  let minArray = [];
-  for (let i in ht) {
-    if (Number(i) > 0) {
-      newArray.push(Number(i));
-    } else {
-      minArray.push(Number(i));
+  let maxNumber = nums[0];
+  let temp = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    temp += nums[i];
+
+    if (temp > maxNumber) {
+      maxNumber = temp;
+    }
+    if (temp < 0) {
+      temp = 0;
     }
   }
-  newArray.push(Math.max(...minArray));
-  const sum = newArray.reduce((ps, cs) => {
-    if (ps + cs <= 0) {
-      if (ps > 0) return ps;
-      else return cs;
-    }
-    return ps + cs;
-  }, 0);
-  return sum;
+  return maxNumber;
 };
