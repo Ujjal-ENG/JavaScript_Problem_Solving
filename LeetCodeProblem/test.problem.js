1a803013a51e58210a9f784efcac568ea6efa9fa
Ujjal Kumar Roy <ujjalroy7862@gmail.comy>
2023-04-02 02:03:08 +0600

    not solved yet

--- LeetCodeProblem/test.problem.js
+++ LeetCodeProblem/test.problem.js
@@ -1,30 +1,14 @@
 var maxSubArray = function (nums) {
-  const ht = {};
+  let currentSum = nums[0];
+  let maxSum = nums[0];
   if (nums.length === 1) {
     return nums[0];
   }
-  for (let i of nums) {
-    ht[i] = ht[i] + 1 || 1;
+  for (let i = 0; i < nums.length; i++) {
+    currentSum = Math.max(currentSum + nums[i], nums[i]);
+    maxSum = Math.max(maxSum, currentSum);
   }
-  let newArray = [];
-  let minArray = [];
-  for (let i in ht) {
-    if (Number(i) > 0) {
-      newArray.push(Number(i));
-    } else {
-      minArray.push(Number(i));
-    }
-  }
-  newArray.push(Math.max(...minArray));
-  console.log(newArray);
-  const sum = newArray.reduce((ps, cs) => {
-    if (ps + cs <= 0) {
-      if (ps > 0) return ps;
-      else return cs;
-    }
-    return ps + cs;
-  }, 0);
-  return sum;
+  return maxSum;
 };
 
-console.log(maxSubArray([-1, 1]));
+console.log(maxSubArray([8, -19, 5, -4, 20]));
