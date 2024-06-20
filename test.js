79c4449782de1aa55cc1d11d0aaef3d4d2cfa4ed
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-06-21 01:51:20 +0600

    solved the prime numbers problem

--- test.js
+++ test.js
@@ -1,9 +1,24 @@
 function isPrime(num) {
+    if(typeof num === undefined) return false;
     if(num <= 1) return false;
     for(let i = 2; i <= Math.sqrt(num); i++) {
         if(num % i === 0) return false;
     }
-
     return true;
 }
-console.log(isPrime(5));
+
+function extractNums(nums) {
+    const result = [];
+    for(let i = 0; i < nums.length; i++) {
+       if(isPrime(nums[i])) {
+         result.push(nums[i]+ "is a prime number");
+       }
+       else {
+         result.push(nums[i]+ "is not a prime number");
+       }
+    }
+    return result;
+}
+
+const arrays = [5,7,2,11,3,18,25,24,23]
+console.log(extractNums(arrays));
