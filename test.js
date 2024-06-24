ac33ca2c8fd404ac0e34f0a4769effd3bc3f3a16
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-06-25 00:01:22 +0600

    tring to solving the problem

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
 function positionInverse(nums){
-    let numbers = nums.toString();
-    let resultNum = [];
-    for(let i =0;i<numbers.length;i++){
-        resultNum[i] = numbers[numbers.length - 1 - i];
-    }
-    return resultNum.join('');
+    let count = 0;
+    let position =[];
+    let mod;
+   while(nums > 0){
+    mod = (nums/10).toString().slice(-1);
+    nums = Math.floor(nums / 10);
+    position.push([mod, count+1]);
+    count++;
+   }
+return {count: count, position: position};
 }
 
-console.log(positionInverse(657843687));
+console.log(positionInverse(21453));
+
+//output: 23154
+// P d = a
+// 3 1 = 4
+// 5 2 = 5
+// 4 3 = 1
+// 1 4 = 3
+// 2 5 = 2
