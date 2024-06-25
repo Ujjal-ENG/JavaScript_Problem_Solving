c976421b6e75484026a9016cee741185bed54d0a
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-06-25 15:40:10 +0600

    finnaly solved the reverseNumber based on the position

--- test.js
+++ test.js
@@ -1,26 +1,27 @@
 function positionInverse(nums) {
     let numStr = nums.toString();
     let length = numStr.length;
-    let positions = [];
+    let positionsWithValues = [];
+    let newPositionWithValues = new Array(length);
     
-    // Extract digits and their positions from the number
+    // Extract digits and their positionsWithValues from the number
     for (let i = length - 1; i >= 0; i--) {
         let digit = parseInt(numStr[i]);
-        positions.push(digit, length - i);
+        positionsWithValues.push(digit, length - i);
     }
     
-    // Initialize the new array to store values at their new positions
-    let newPositionArray = new Array(length);
+    // // Initialize the new array to store values at their new positions
+    // let newPositionArray = new Array(length);
 
-    // Place digits at their correct positions
-    for (let i = 0; i < positions.length; i += 2) {
-        let value = positions[i];
-        let pos = positions[i + 1];
-        newPositionArray[pos - 1] = value;
+    // // Place digits at their correct positions
+    for (let i = 0; i < positionsWithValues.length; i += 2) {
+        let pos = positionsWithValues[i];
+        let value = positionsWithValues[i + 1];
+        newPositionWithValues[pos-1] = value;
     }
 
-    // Join the array into a string to get the final result
-    let result = newPositionArray.join('');
+    // // Join the array into a string to get the final result
+    let result = newPositionWithValues.reverse().join('');
 
     return result;
 }
