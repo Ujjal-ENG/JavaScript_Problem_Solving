e052ecedb63293f7263aeaea063de8507488e206
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-06-26 00:46:29 +0600

    started to solving the rotateNumber problem

--- test.js
+++ test.js
@@ -1,28 +1,8 @@
-function positionInverse(nums) {
-    let numStr = nums.toString();
-    let length = numStr.length;
-    let positions = [];
+function rotateNumber(number, direction) {
     
-    // Extract digits and their positions from the number
-    for (let i = length - 1; i >= 0; i--) {
-        let digit = parseInt(numStr[i]);
-        positions.push(digit, length - i);
+    for(let i=1;i<=direction;i++){
+
     }
-    
-    // Initialize the new array to store values at their new positions
-    let newPositionArray = new Array(length);
-
-    // Place digits at their correct positions
-    for (let i = 0; i < positions.length; i += 2) {
-        let value = positions[i];
-        let pos = positions[i + 1];
-        newPositionArray[pos - 1] = value;
-    }
-
-    // Join the array into a string to get the final result
-    let result = newPositionArray.join('');
-
-    return result;
 }
 
-console.log(positionInverse(21453)); // Output: 23154
+console.log(rotateNumber(12345, 2)); // Output: 54123
