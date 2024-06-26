f83ab7bb53cb1d6010811c05add11ce45eb59b67
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-06-26 23:34:48 +0600

    same problem solved different and optimized way

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
 var differenceOfSums = function(n, m) {
     let sum1 = 0;
-    for(let i = 1; i <= n; i++) {
-        if(i % m !== 0){
-            sum1 += i;
-        }
-    }
     let sum2 = 0;
     for(let i = 1; i <= n; i++) {
         if(i%m ===0){
             sum2 += i;
+        }else{
+            sum1 += i;
         }
     }
     return sum1 - sum2;
