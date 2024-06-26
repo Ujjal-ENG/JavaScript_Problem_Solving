43e632f978c864a5bac51240783e2d2469614515
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-06-26 21:55:39 +0600

    solved the rotate number problem

--- test.js
+++ test.js
@@ -1,8 +1,18 @@
-function rotateNumber(number, direction) {
-    
-    for(let i=1;i<=direction;i++){
-
+function generateTheZero(number) {
+    let generateNumber = 1;
+    for(let i=1;i<=number;i++){
+        generateNumber+="0";
     }
+    return Number(generateNumber)
 }
 
-console.log(rotateNumber(12345, 2)); // Output: 54123
+function rotateNumber(number, direction) {
+    if(number === 0) return 0;
+    if(String(number).length === direction) return number;
+  const zero = generateTheZero(direction);
+  const [firstPart,rest] = String(number/zero).split('.');
+  return Number(`${rest}${firstPart}`);
+}
+
+
+console.log(rotateNumber(12345, 5)); // Output: 54123
