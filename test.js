4d8f2aaacd5329e79f8698e7a008538ae7086227
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-06-30 10:27:55 +0600

    solved 2723 number problem in leetcode

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-function argumentLength(...args){
-  return args.length;
-}
+var addTwoPromises = async function(promise1, promise2) {
+    const result1 = await promise1.then((res)=>res)
+    const result2 = await promise2.then((res)=>res)
+    console.log(result1+result2)
+};
+let promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20))
+let promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60))
 
-console.log(argumentLength({}, null, "3"))
+console.log(addTwoPromises(promise1,promise2))
