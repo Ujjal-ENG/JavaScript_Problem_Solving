3f5a0937587f780c5fc241d56b98ad7637519674
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-06-30 10:58:54 +0600

    solved another one problem

--- test.js
+++ test.js
@@ -1,9 +1,5 @@
-var addTwoPromises = async function(promise1, promise2) {
-    const result1 = await promise1.then((res)=>res)
-    const result2 = await promise2.then((res)=>res)
-    console.log(result1+result2)
+var theMaximumAchievableX = function(num, t) {
+    return num+t*2;
 };
-let promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20))
-let promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60))
 
-console.log(addTwoPromises(promise1,promise2))
+theMaximumAchievableX(5,1);
