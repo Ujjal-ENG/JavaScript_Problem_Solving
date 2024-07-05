fcc6d03dea85596620fd13b0186e507d04a62906
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-07-06 00:06:28 +0600

    tring to solved the remove duplicated one

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
-function generateArray(){
+var containsNearbyDuplicate = function(nums, k) {
+    for(let i=0;i<nums.length;i++){
+      for(let j=0;j<nums.length;j++){
+        if((nums[i]==nums[j]) && Math.abs(i-j) >=k){
+          return true;
+        }
+      }
+    }
+    return false;
+};
 
-}
-
-console.log(generateArray(5));
+console.log(containsNearbyDuplicate([1,2,3,1],3));
