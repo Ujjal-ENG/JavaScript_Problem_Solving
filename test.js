a6134baa4f92f789c9f4f3ac47c1a0b6adb0a6b9
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-07-05 23:26:08 +0600

    started again the problem solving

--- test.js
+++ test.js
@@ -1,14 +1,5 @@
-function barChartFuntion(arrs){
-  for(let i of arrs){
-    console.log(makeStar(i));
-  }
-}
-function makeStar(num){
-  let star = "";
-  for(let i = 0; i < num; i++){
-    star += "*";
-  }
-  return star;
+function generateArray(){
+
 }
 
-barChartFuntion([4,3,2,0,5,3])  
+console.log(generateArray(5));
