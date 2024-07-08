52f7899f8b483c129671dd24f81bd9af96a2ff3a
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-07-08 22:02:25 +0600

    started the new one

--- test.js
+++ test.js
@@ -1,12 +1,3 @@
-var containsNearbyDuplicate = function(nums, k) {
-    for(let i=0;i<nums.length;i++){
-      for(let j=0;j<nums.length;j++){
-        if((nums[i]==nums[j]) && Math.abs(i-j) >=k){
-          return true;
-        }
-      }
-    }
-    return false;
+var minimumOperations = function(nums) {
+    
 };
-
-console.log(containsNearbyDuplicate([1,2,3,1],3));
