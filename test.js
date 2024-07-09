ec53ea2eb414f99704274c3b3ebec71b5b7f386d
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-07-09 22:45:50 +0600

    done 100th problem

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 var findWordsContaining = function(words, x) {
     let result = [];
-    for(let [word,ins] of words) {
-      if(word.includes(x)) result.push(ins[0]);
+    for(let i = 0; i < words.length; i++) {
+        if(words[i].includes(x)) {
+            result.push(i);
+        }
     }
     return result;
 };
