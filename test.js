8515aeb58f087cc6120e663c51d942019c6e9923
Ujjal <ujjalroy7862@gmail.com>
2024-07-09 17:56:51 +0600

    brtueforcelly trying to solved the problem

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
-var minimumOperations = function(nums) {
-    
+var findWordsContaining = function(words, x) {
+    let result = [];
+    for(let word of words) {
+      if(word.includes(x)) result.push(word[0]);
+    }
+    return result;
 };
+
+console.log(findWordsContaining(["abc","bcd","aaaa","cbc"],"a"));
