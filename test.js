f7007dd64c0a57eed2db3a053b045d4769d749d7
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-07-11 23:04:07 +0600

    started the solve duplicate bracktes problem

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-var findWordsContaining = function(words, x) {
-    let result = [];
-    for(let i = 0; i < words.length; i++) {
-        if(words[i].includes(x)) {
-            result.push(i);
-        }
-    }
-    return result;
-};
-
-console.log(findWordsContaining(["abc","bcd","aaaa","cbc"],"a"));
+function duplicateBrackets(bracs){
+    
+}
