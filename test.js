750221cc8881d21a6fdbdc877afda6ca78d6d099
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-07-12 00:04:59 +0600

    stack peek function maked

--- test.js
+++ test.js
@@ -1,3 +1,22 @@
 function duplicateBrackets(bracs){
+    let stack = [];
+    let result = [];
     
+    for(let i = 0; i < bracs.length; i++){
+        if(bracs[i] === '('){
+            stack.push(i);
+            console.log(peek(stack));
+        } else if(bracs[i] === ')'){
+            if(stack.length > 0){
+                result.push([stack.pop(), i]);
+            }
+        }
+    }
+    return result;
 }
+
+function peek(arry){
+    return arry[arry.length - 1];
+}
+
+console.log(duplicateBrackets("(1+2)+((1+2))"));
