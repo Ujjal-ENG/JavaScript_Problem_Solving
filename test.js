4f0a5ce954a807fb899975cba04f94ee120363eb
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-07-15 23:53:42 +0600

    started again the project

--- test.js
+++ test.js
@@ -1,26 +1,5 @@
-function duplicateBrackets(exp){
-    let stack = [];
+function findDuplicates(nums){
 
-    for(let ch of exp){
-       if(ch === ")"){
-        if(stack[stack.length-1] ==="("){
-            return true;
-        }else{
-            while(stack.length > 0 && stack[stack.length-1] !=="("){
-                stack.pop();
-            }
-            if(stack.length > 0){
-                stack.pop();
-            }
-        }
-       }else{
-        stack.push(ch);
-       }
-    }
-    return false;
 }
-// Example usage:
-console.log(duplicateBrackets("(1+2)+((1+2))")); // Output: true
-console.log(duplicateBrackets("(1+2) + (3+4)")); // Output: false
-console.log(duplicateBrackets("((a + b))"));     // Output: true
-console.log(duplicateBrackets("(a + (b + c))")); // Output: false
+
+console.log(findDuplicates([1, 2, 3, 4, 5, 2, 3])); // [2, 3]
