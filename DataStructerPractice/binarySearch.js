28610f455bcf25e7387a42d2597e3485daed0863
Ujjal Kumar Roy <63545258+Ujjal-ENG@users.noreply.github.com>
2024-07-25 12:04:46 +0600

    Update binarySearch.js

--- DataStructerPractice/binarySearch.js
+++ DataStructerPractice/binarySearch.js
@@ -1,6 +1,6 @@
 function binarySearch(arrs, value) {
-  // let min = 0;
-  // let max = arrs.length - 1;
+   let min = 0;
+   let max = arrs.length - 1;
 
   // while (min <= max) {
   //   let middle = Math.floor((min + max) / 2);
