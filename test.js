b6e432f347e0b804ed86c81207d72682c3ecd466
ujjal_roy <ujjalroy7862@gmail.com>
2024-08-30 22:53:04 +0600

    solved the another pattern problem

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 function patternShowing(num) {
-    for (let i = 1; i <= num; i++) {
+    for (let i = num; i >=0; --i) {
         let line = '';
         for (let j = 0; j < i; j++) {
             line += i;
