73af85b903468de38ab1cef038b8c7bc042ccfd2
ujjal_roy <ujjalroy7862@gmail.com>
2024-09-02 00:53:16 +0600

    added

--- test.js
+++ test.js
@@ -1,15 +1,10 @@
 function patternShowing(num) {
-    let line = '*';
+    let line = ' ';
     for (let i = 1; i <=num; i++) {
-        for (let j = 1; j <= i; j++){
-            if (i == 1) {
-                console.log('*');
-            }
-            else {
-                let star = (j * 2) + 1;
-                console.log('*'.repeat(star));
-            }
+        for (let j = 0; j <= i; j++){
+            console.log(line +'*')
         }
+        console.log(line);
     }
 }
 
