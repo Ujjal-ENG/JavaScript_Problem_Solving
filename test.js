869f89092afa83f2b314d83f7d3704487e1be7b8
ujjal_roy <ujjalroy7862@gmail.com>
2024-09-07 23:25:31 +0600

    sorting the time order

--- test.js
+++ test.js
@@ -1,11 +1,28 @@
-function patternShowing(num) {
-    let line = '';
-    for (let i = 0; i < num; i++) {
-        for (let j = 0; j < i; j++){
-            line += "*";
-        }
-        console.log(line);
+function minDaysToBecomeEngineers(n, trTimes, sLTimes) {
+
+    let joiners = trTimes.map((trTime, index) => ({
+        trTime: trTime,
+        sLTime: sLTimes[index]
+    }));
+    
+  
+    joiners.sort((a, b) => b.sLTime - a.sLTime);
+    
+    let currentDay = 0;
+    let endDay = 0;
+    
+    for (let joiner of joiners) {
+        currentDay += joiner.trTime;
+        endDay = Math.max(endDay, currentDay + joiner.sLTime);
     }
+    
+    return endDay;
 }
 
-patternShowing(4);
+// Sample input
+const n = 3;
+const trainingTimes = [2, 5, 4];
+const selfLearningTimes = [3, 4, 2];
+
+
+console.log(minDaysToBecomeEngineers(n, trainingTimes, selfLearningTimes)); 
